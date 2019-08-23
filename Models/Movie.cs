8692c9882fecb52b20286717c695ef277cebdf3c
Samuel Solberg <pauliesays65@gmail.com>
2019-08-23 16:09:00 -0500

    added database context class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,10 +5,10 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
-        public int ID { get; set; }
+        public int ID { get; set; } // Required by DB for primary key
         public string Title { get; set; }
 
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date)] // Specifies type of data (Date)
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
