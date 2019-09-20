c7f1330021957e70adfcb509b18c600d4c91b4d5
Samuel Solberg <pauliesays65@gmail.com>
2019-09-20 16:06:05 -0500

    added DB seed  migration to Program.cs file

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
 
-namespace RazorPagesMovie.Model 
+namespace RazorPagesMovie.Models 
 {
     public static class SeedData
     {
